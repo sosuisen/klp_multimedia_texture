44fc7b759bbb37bcef98f02f4e3c95bfee7f6873
sosuisen <hidekazu.kubota@gmail.com>
2023-02-26 00:09:13 +0900

    fix: remove hints

--- myfilter.js
+++ myfilter.js
@@ -1,18 +1,5 @@
 PIXI.filters.MyFilter = class extends PIXI.Filter {
   constructor() {
-    // colorには元の画像のピクセルの色が収められています。
-    //
-    // 今回、元の画像に対して加算（Add）したい色は #909000 です。
-    // rgbを0から255までの数値で表現すると、rgb(144, 144, 0)です。
-    // 
-    // 一方、シェーダでは色は0.0から1.0までの範囲で表現されます。
-    // たとえば、255は1.0、51は0.2となります。よって、
-    // color.r += 0.2;
-    // と書くと、rに51を加算（Add）するのと同じです。
-    // 
-    // 式に数値を書くときはは 144.0 のように必ず浮動小数点数で書いてください。
-    // 書く色の値が 1.0 を超えたら 1.0 にしてください。
-
     const fragmentSrc = `
         precision mediump float;
         varying vec2 vTextureCoord;
