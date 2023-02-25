f9aca193c69132df40f763cbe8f61fc8a8f3ecfd
sosuisen <hidekazu.kubota@gmail.com>
2023-02-25 11:58:10 +0900

    first commit

---
+++ texture.js
@@ -0,0 +1,62 @@
+let app = new PIXI.Application({ antialias: true, width: 640, height: 480 });
+gsap.registerPlugin(PixiPlugin);
+app.ticker.stop();
+gsap.ticker.add(time => {
+  app.ticker.update();
+});
+
+document.body.appendChild(app.view);
+
+// タイムライン
+const tl = gsap.timeline()
+
+// コンテナの作成
+const container = new PIXI.Container();
+app.stage.addChild(container);
+
+// テクスチャの作成
+const texture = PIXI.Texture.from('kyoco_small.png');
+// テクスチャからスプライトを作成
+const sprite1 = new PIXI.Sprite(texture);
+container.addChild(sprite1);
+tl.to(sprite1, { x: 520, duration: 2, repeat: -1, ease: 'power1.inOut', yoyo: true });
+
+// テクスチャは他のスプライトでも使い回すことができます。
+// 一方、PIXI.Sprite.from()では、毎回新たにメモリが確保されてしまいます。
+const sprite2 = new PIXI.Sprite(texture);
+sprite2.position.set(520, 150);
+container.addChild(sprite2);
+tl.to(sprite2, { x: 0, duration: 2, repeat: -1, ease: 'power1.inOut', yoyo: true }, '<');
+
+// 動画もテクスチャにできます（音も鳴ります）
+// 「動画再生」ボタンで再生
+const vTexture = PIXI.Texture.from('nhk.mp4');
+vTexture.baseTexture.resource.autoPlay = false;
+const sprite3 = new PIXI.Sprite(vTexture);
+// １番最初（0）の子、つまり１番奥へ追加
+container.addChildAt(sprite3, 0);
+// タイムライン開始の0秒後に開始
+tl.to(sprite3, { x: -100, duration: 2, repeat: -1, ease: 'power1.inOut', yoyo: true }, '<');
+
+const mask = new PIXI.Graphics();
+mask.beginFill();
+// mask.drawCircle(200, 200, 200);
+mask.drawCircle(370, 150, 100);
+mask.endFill();
+
+// 動画にマスクを追加すると、動画だけがクリッピングされます。
+sprite3.mask = mask;
+
+// さらにマスクを動画の子にすると、動画とともに移動します。
+sprite3.addChild(mask);
+
+// さらにマスクをコンテナの子にすると、コンテナ全体がクリッピングされます。
+container.mask = mask;
+
+app.stage.addChild(container);
+
+/**
+ * 動画の一時停止・再生
+ */
+document.getElementById('pauseBtn').addEventListener('click', () => vTexture.baseTexture.resource.source.pause());
+document.getElementById('resumeBtn').addEventListener('click', () => vTexture.baseTexture.resource.source.play());
