461f91cfb98bc6a6cac11fab597e9740cd367267
sosuisen <hidekazu.kubota@gmail.com>
2023-03-06 00:52:04 +0900

    fix: answer

--- blend.js
+++ blend.js
@@ -78,6 +78,6 @@ bg5.tint = color;
  * 発展課題
  */ 
 // カスタムフィルタを適用
-const myFilter = new MyFilter();
+const myFilter = new MyFilter(144, 144, 0);
 bg6.filters = [myFilter];
 bg6.addChild(new PIXI.Text('Add（発展課題）', style));
--- myfilter.js
+++ myfilter.js
@@ -1,15 +1,16 @@
 import * as PIXI from 'pixi.js';
 
 export const MyFilter = class extends PIXI.Filter {
-  constructor() {
+  constructor(r, g, b) {
     const fragmentSrc = `
         varying vec2 vTextureCoord;
         uniform sampler2D uSampler;
   
         void main(void) {
           vec4 color = texture2D(uSampler, vTextureCoord);
-          color.r = min(color.r + 144.0/255.0, 1.0);
-          color.g = min(color.g + 144.0/255.0, 1.0);
+          color.r = min(color.r + ${r}.0/255.0, 1.0);
+          color.g = min(color.g + ${g}.0/255.0, 1.0);
+          color.b = min(color.b + ${b}.0/255.0, 1.0);
           gl_FragColor = color;
         }
       `;
